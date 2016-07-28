0ef5981124a444d1061e99d572f8e82236df622b
Salwador Szurlej <sal.szu@gmail.com>
2016-07-28 11:18:17 -0400

    merged two local files and updated solution

--- main.js
+++ main.js
@@ -1,2 +1,62 @@
 //contains the logic of the app. Running this in terminal/bash will start the game
 //app should end when a player guesses the correct word or runs out of guesses
+
+var prompt = require('prompt');
+var Word = require('./word.js');
+
+prompt.start();
+
+game = {
+	wordBank : ["delicious", "repulsive", "attractive", "disgusting", "terrifying", "tasty"],
+	wordsWon : 0,
+	guessesRemaining : 10, 
+	currentWrd : null, 
+	startGame : function (wrd){
+		this.resetGuessesRemaining();
+		this.currentWrd = new Word(this.wordBank[Math.floor(Math.random()* this.wordBank.length)]);
+		this.currentWrd.getLets(); 
+		this.keepPromptingUser();
+
+	}, 
+	resetGuessesRemaining : function(){
+		this.guessRemaining = 10;
+	},
+	keepPromptingUser : function(){
+		var self = this;
+
+		prompt.get(['guessLetter'], function(err, result) {
+		  console.log('  The letter or space you guessed is: ' + result.guessLetter);
+
+		    var findHowManyOfUserGuess = self.currentWrd.checkIfLetterFound(result.guessLetter);
+		    if (findHowManyOfUserGuess == 0){
+		    	console.log('You guessed wrong!');
+		    	self.guessesRemaining--;
+		    }else{
+		    	console.log('You guessed right!');
+
+	    		if(self.currentWrd.didWeFindTheWord()){
+			    	console.log('You Won!!!');
+			    	return; 
+			    }
+		    }
+		    
+		    console.log('Guesses remaining: ', self.guessesRemaining);
+		    console.log(self.currentWrd.wordRender());
+		    console.log('here are the letters you guessed already: ');
+
+		    if ((self.guessesRemaining > 0) && (self.currentWrd.found == false)){
+		    	self.keepPromptingUser();
+		    }
+		    else if(self.guessesRemaining == 0){
+		    	console.log('Game over bro it was ', self.currentWrd.word);
+		    	console.log('Get with the program man');
+		    }else{
+		    	console.log(self.currentWrd.wordRender());
+		    }
+		});
+	}
+
+
+};
+
+game.startGame();
--- word.js
+++ word.js
@@ -1,2 +1,49 @@
 // constructor file
 //contains all of the methods which will check the letter guessed versus the random word selected
+
+var Letter = require('./letter.js');
+
+var Word = function(wrd){
+	this.word = wrd;
+	this.lets = []; 
+	this.found = false;
+
+	this.getLets = function() {
+		for(var i = 0; i < this.word.length; i++) {
+			this.lets.push(new Letter(this.word[i]));
+		}
+	};
+	
+	this.didWeFindTheWord = function() {
+		this.found = this.lets.every(function(curLet) {
+			return curLet.appear;
+		});
+
+		return this.found;
+	};
+
+	this.checkIfLetterFound = function(guessLetter) {
+		var whatToReturn = 0;
+
+		for(var i = 0; i < this.lets.length; i++) {
+			if (this.lets[i].charac == guessLetter){
+				this.lets[i].appear = true;
+				whatToReturn++;
+			}
+		}
+
+		return whatToReturn;
+	};
+
+	this.wordRender = function() {
+		var str = '';
+
+		for(var i=0; i < this.lets.length; i++){
+			str += this.lets[i].letterRender();
+		}
+
+		return str;
+	};
+}
+
+module.exports = Word;
